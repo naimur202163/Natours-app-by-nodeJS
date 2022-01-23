1e9655ff77e31b6ce22b14a281e41f5a363a7871
unknown <you@example.com>
2022-01-23 12:44:32 +0600

    server is fine

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const fs = require('fs');
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello From the server');
 });
@@ -18,6 +19,38 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+// Doplicate
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
+// Adding post Method
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours, (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    })
+  );
 });
 
 app.listen(port, () => {
